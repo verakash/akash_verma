75e2b5c10bb41c195cfad93f82929a4cbd6f147b
verakash <akashverma150995@gmail.com>
2021-03-23 14:15:31 +0530

    modified models.py

--- my_app/models.py
+++ my_app/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class testmodels(models.Model):
     Name = models.CharField(max_length= 20)
-    Age= models.IntegerField(max_length= 3)
+    Age= models.IntegerField(blank=False, null=False)
+
 
     def __str__(self):
         return self.Name
